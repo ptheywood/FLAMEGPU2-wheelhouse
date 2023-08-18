5e92add9efa231f4af92c6a9e8557461e197875c
Peter Heywood <peethwd@gmail.com>
2023-08-18 12:07:29 +0100

    Support more than 30 releases, and reduce api query count

--- scripts/get_release_json.py
+++ scripts/get_release_json.py
@@ -29,28 +29,16 @@ def main():
     if gh_check.returncode != 0:
         raise Exception(f"Error occurred checking 'gh' usage. Please install 'gh' and ensure it is on your path")
     
-    # use gh to query the repo for a list of releases
-    gh_list_releases_command = f"gh api -X GET repos/{GHREPO}/releases --jq '[ .[]|.tag_name ]'"
+    # use gh to query the repo for releases including assets. 
+    # --paginate means if more than one api call is needed it will make it.
+    # Use jq to select non-draft releases, and filter output to include the name of the release, and the per asset name and url.
+    # Note the split jq string to avoid escaping braces
+    jq_command = "'[ .[] | select(.draft==false) | {release: .tag_name, wheels: [.assets[] | {name:.name, url:.browser_download_url } ] } ]'"
+    gh_list_releases_command = f"gh api -X GET repos/{GHREPO}/releases --paginate --jq {jq_command}"
     gh_list_releases = subprocess.run(gh_list_releases_command, shell=True, capture_output=True)
     if gh_list_releases.returncode != 0:
         raise Exception(f"Error occured while fetching list of releases {gh_list_releases.stderr.decode()}")
-    releases_list = json.loads(gh_list_releases.stdout.decode())
-
-    # for each release, query gh for the assets and build the output data structure
-    data = []
-    for idx, tag in enumerate(releases_list):
-        # escaping needed here is kinda grim
-        gh_list_assets_command = f'gh -R {GHREPO} release view {tag} --json assets --jq "[ .assets.[] | select(.name|endswith(\\".whl\\")) | {{name,url}} ]"'
-        # print(gh_list_assets_command)
-        gh_list_assets = subprocess.run(gh_list_assets_command, shell=True, capture_output=True)
-        if gh_list_assets.returncode != 0:
-            raise Exception(f"Error occured while fetching list of releases {gh_list_assets.stderr.decode()}")
-        release_assets = json.loads(gh_list_assets.stdout.decode())
-        release_obj = {
-            "release": tag, 
-            "wheels": release_assets
-        }
-        data.append(release_obj)
+    data = json.loads(gh_list_releases.stdout.decode())
 
     # output json data to disk
     with open(args.json_path, "w") as fp:
