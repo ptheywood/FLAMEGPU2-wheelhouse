67afee1539e5d6bdd19a71c9b66ec895746a94b7
Peter Heywood <peethwd@gmail.com>
2023-08-18 16:57:37 +0100

    --index-url compliance, plus separate base instructions root

--- readme.md
+++ readme.md
@@ -5,14 +5,18 @@ Repository for generating and hosting a python wheelhouse, for non pypi complian
 `main` contains:
 
 + `scripts/get_release_json.py` - uses `gh` to query the `FLAMEGPU/FLAMEGPU2` repository for releases and their assets, storing to JSON on disk
-+ `scripts/build_wheelhouse.py` - Generate a html file compliant with `pip install`'s `-f, --find-links <url>` option from the previously stored JSON
++ `scripts/build_wheelhouse.py` - Generate a html files compliant with `pip install`'s `-f, --find-links <url>`, `-i, --index-url <url>` & `--extra-index-url <url>` options from the previously stored JSON
 
     ```text
-    -f, --find-links <url>      If a URL or path to an html file, 
-    then parse for links to archives such as sdist (.tar.gz) or wheel (.whl) files. 
-    If a local path or file:// URL that's a directory, 
-    then look for archives in the directory listing.
-    Links to VCS project URLs are not supported.
+    -i, --index-url <url>       Base URL of the Python Package Index (default https://pypi.org/simple). This should
+                                point to a repository compliant with PEP 503 (the simple repository API) or a local
+                                directory laid out in the same format.
+    --extra-index-url <url>     Extra URLs of package indexes to use in addition to --index-url. Should follow the same
+                                rules as --index-url.
+    --no-index                  Ignore package index (only looking at --find-links URLs instead).
+    -f, --find-links <url>      If a URL or path to an html file, then parse for links to archives such as sdist
+                                (.tar.gz) or wheel (.whl) files. If a local path or file:// URL that's a directory, then
+                                look for archives in the directory listing. Links to VCS project URLs are not supported.
     ```
 
 Via GitHub actions, these scripts can be executed, and the resulting HTML file(s) deployed to gh pages, making it publicly accessible on the internet.
@@ -23,6 +27,69 @@ Via GitHub actions, these scripts can be executed, and the resulting HTML file(s
 + `gh`, with sufficient privileges
 + Internet connection
 
+## Usage
+
+```bash
+# via gh, get release and asset info, storing into pyflamegpu.json
+./scripts/get_release_json.py pyflamegpu.json
+# Generate html containing links to each gh hosted wheel, into _build
+./scripts/builds_wheelhouse.py -i pyflamegpu.json -o sample/wheelhouse
+```
+
+It is then possible to install pyflamegpu and specific versions via pip using `-f, --find-links <url>`, `-i, --index-url <url>` & `--extra-index-url <url>`
+
+I.e. Using the version hosted at [`whl.flamegpu.com`](https://whl.flamegpu.com) via `-i, --index-url`
+
+```bash
+# Install the "newest" version, so most recent with newest CUDA and with visualiastion, via -f
+python3 -m pip install --index-url https://whl.flamegpu.com/whl/ pyflamegpu
+# Install a specific version, with the most recent CUDA available and with visualisation
+python3 -m pip install --index-url https://whl.flamegpu.com/whl/ pyflamegpu==2.0.0rc0
+# Install the most recent non-visualisation build, using a specific cuda version, in this case CUDA 11.2
+python3 -m pip install --index-url https://whl.flamegpu.com/whl/cuda112/ pyflamegpu
+# Install the most recent visualisation build, using a specific cuda version, in this case CUDA 11.0
+python3 -m pip install --index-url https://whl.flamegpu.com/whl/cuda110-vis/ pyflamegpu
+```
+
+Or using the version hosted at [`whl.flamegpu.com`](https://whl.flamegpu.com) via `-f, --find-links`
+
+```bash
+# Install the "newest" version, so most recent with newest CUDA and with visualiastion, via -f
+python3 -m pip install -f https://whl.flamegpu.com/whl/pyflamegpu/ pyflamegpu
+# Install a specific version, with the most recent CUDA available and with visualisation
+python3 -m pip install -f https://whl.flamegpu.com/whl/pyflamegpu/ pyflamegpu==2.0.0rc0
+# Install the most recent non-visualisation build, using a specific cuda version, in this case CUDA 11.2
+python3 -m pip install -f https://whl.flamegpu.com/whl/cuda112/pyflamegpu/ pyflamegpu
+# Install the most recent visualisation build, using a specific cuda version, in this case CUDA 11.0
+python3 -m pip install -f https://whl.flamegpu.com/whl/cuda110-vis/pyflamegpu/ pyflamegpu
+```
+
+
+Or using a local copy of the wheelhouse via `-i, --index-url`
+
+```bash
+# Install the "newest" version, so most recent with newest CUDA and with visualiastion, via -f
+python3 -m pip install --index-url sample/wheelhouse/whl/ pyflamegpu
+# Install a specific version, with the most recent CUDA available and with visualisation
+python3 -m pip install --index-url sample/wheelhouse/whl/ pyflamegpu==2.0.0rc0
+# Install the most recent non-visualisation build, using a specific cuda version, in this case CUDA 11.2
+python3 -m pip install --index-url sample/wheelhouse/whl/cuda112/ pyflamegpu
+# Install the most recent visualisation build, using a specific cuda version, in this case CUDA 11.0
+python3 -m pip install --index-url sample/wheelhouse/whl/cuda110-vis/ pyflamegpu
+```
+
+Or using a local copy of the wheelhouse via `-f, --find-links`
+
+```bash
+# Install the "newest" version, so most recent with newest CUDA and with visualiastion, via -f
+python3 -m pip install -f sample/wheelhouse/whl/pyflamegpu/ pyflamegpu
+# Install a specific version, with the most recent CUDA available and with visualisation
+python3 -m pip install -f sample/wheelhouse/whl/pyflamegpu/ pyflamegpu==2.0.0rc0
+# Install the most recent non-visualisation build, using a specific cuda version, in this case CUDA 11.2
+python3 -m pip install -f sample/wheelhouse/whl/cuda112/pyflamegpu/ pyflamegpu
+# Install the most recent visualisation build, using a specific cuda version, in this case CUDA 11.0
+python3 -m pip install -f sample/wheelhouse/whl/cuda110-vis/pyflamegpu/ pyflamegpu
+```
 
 ## CI
 
--- scripts/build_wheelhouse.py
+++ scripts/build_wheelhouse.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 """
-Provided a path to a JSON file containing per release wheel names and URLS, output a html wheelhouse for pip consumption via -f, --find-links <url>
+Provided a path to a JSON file containing per release wheel names and URLS, output a html wheelhouse for pip consumption via -f, --find-links <url> or --index-url <url>
 """
 
 import argparse
 import pathlib
 import json
 import re
+
+def pep503_normalize(name):
+    return re.sub(r"[-_.]+", "-", name).lower()
 
 def cli():
     # CLI
@@ -16,6 +19,7 @@ def cli():
     parser.add_argument("-o", "--output-dir", type=pathlib.Path, required=True, help="Path for html directory output")
     # add --many-wheelhouses/--no-many-wheelhouses, creating a directory / html file build per cuda version and per vis / non vis.
     parser.add_argument('--many-wheelhouses', default=True, action=argparse.BooleanOptionalAction, help="Enable/Disable creation of separate html files per local version component. Enabled by default")
+    parser.add_argument("--baseurl", type=str, help="Base url to embed within the installation instructions. Include the trailing /.")
     args =  parser.parse_args()
 
     # Validate CLI
@@ -61,18 +65,18 @@ def build_split_files_anchor_list(args, data):
                 cuda_match = cuda_version_re.search(wheel_name)
                 cuda_version = cuda_match.group(0) if cuda_match else "cuda110" # only version without an explicit cuda version is cuda 11.0 as of rc0. This might need adjusting in the future
                 vis_match = vis_re.search(wheel_name)
-                is_vis = "vis" if vis_match else ""
-                subdir = pathlib.Path(cuda_version) / is_vis
+                is_vis = "-vis" if vis_match else ""
+                subdir = f"{cuda_version}{is_vis}"
                 if subdir not in split_files_dict:
                     split_files_dict[subdir] = []
                 split_files_dict[subdir].append(f"        <a href=\"{wheel_url}\">{wheel_name}</a>")
     return split_files_dict
 
 def write_anchor_list_to_disk(filepath, a_list):
-        # Join th elist of anchors with line breaks and new lines
-        anchors = "<br />\n".join(a_list)
-        # Create the final html output
-        html = f"""<!DOCTYPE html>
+    # Join th elist of anchors with line breaks and new lines
+    anchors = "<br />\n".join(a_list)
+    # Create the final html output
+    html = f"""<!DOCTYPE html>
 <html>
     <body>
         <h1>pyflamegpu wheel urls</h1>
@@ -80,9 +84,61 @@ def write_anchor_list_to_disk(filepath, a_list):
     </body>
 </html>"""
 
-        filepath.parent.mkdir(parents=True, exist_ok=True)
-        with open(filepath, "w") as fp:
-            fp.write(html)
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    with open(filepath, "w") as fp:
+        fp.write(html)
+
+def write_project_list_to_disk(filepath, projects):
+    a_list = [f"    <a href=\"{pep503_normalize(pname)}/\">{pname}</a>" for pname in projects]
+    anchors = "<br />\n".join(a_list)
+    html = f"""<!DOCTYPE html>
+<html>
+  <body>
+    {anchors}
+  </body>
+</html>"""
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    with open(filepath, "w") as fp:
+        fp.write(html)
+
+
+def write_installation_instructions(filepath, sample_package, descriptions):
+    i_tr_list = []
+    f_tr_list = []
+    for desc, i_uri, f_uri in descriptions:
+        i_tr_list.append(f"        <tr><td>{desc}/</td><td><code>python3 -m pip install --index-url {i_uri} {sample_package}</code></td></tr>")
+        f_tr_list.append(f"        <tr><td>{desc}/</td><td><code>python3 -m pip install --find-links {f_uri} {sample_package}</code></td></tr>")
+    i_tablerows = "\n".join(i_tr_list)
+    f_tablerows = "\n".join(f_tr_list)
+    html = f"""<!DOCTYPE html>
+<html>
+  <head>
+  <title>pyflamegpu wheels</title>
+  </head>
+  <body>
+    <h1><code>pyflamegpu</code> Wheels</h1>
+    <p><code>pyflamegpu</code> wheels can be installed via pip using the <code>-i, --index-url<code>, <code>--extra-index-url<code> or <code>-f, --find-links</code> arguments, and one of the uri's provided.<p>
+    <h2>Using <code>-i, --index-url<code>, <code>--extra-index-url<code></h2>
+    <table style="text-align:left;">
+        <tr>
+            <th>Description</th>
+            <th>Command</th>
+        </tr>
+        {i_tablerows}
+    </table>
+    <h2>Using <code>-f, --find-links</code></h2>
+    <table style="text-align:left;">
+        <tr>
+            <th>Description</th>
+            <th>Command</th>
+        </tr>
+        {f_tablerows}
+    </table>
+  </body>
+</html>"""
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    with open(filepath, "w") as fp:
+        fp.write(html)
 
 def main():
     # Parse CLI
@@ -93,11 +149,40 @@ def main():
     full_a_list = build_full_anchor_list(args, data)
     # Build dictionary of anchors for each "split wheelhouse" 
     split_files_a_list_dict = build_split_files_anchor_list(args, data)
+    # @todo - this might need to be dynamic for a0 vis wheels 
+    projects = ["pyflamegpu"]
+    whl_subdir = "whl"
+    whl_dir = args.output_dir / whl_subdir
+    descriptions = []
+
     # Write the main wheelhouse to disk
-    write_anchor_list_to_disk(args.output_dir / "index.html", full_a_list)
+    uri_base = f"{whl_subdir}"
+    if args.baseurl is not None and len(args.baseurl) > 0:
+        uri_base = f"{args.baseurl}/{whl_subdir}" if not args.baseurl.endswith("/") else f"{args.baseurl}{whl_subdir}"
+    write_project_list_to_disk(whl_dir / "index.html", projects)
+    write_anchor_list_to_disk(whl_dir / projects[0] / "index.html", full_a_list)
+    descriptions.append([
+        f"All builds of {projects[0]}. Defaults to latest CUDA with visualisation",
+        f"{uri_base}/",
+        f"{uri_base}/{projects[0]}/",
+    ])
     # Write each child wheelhouse to disk
-    for subdir, sub_a_list in split_files_a_list_dict.items():
-        write_anchor_list_to_disk(args.output_dir / subdir / "index.html", sub_a_list)
+    cuda_re = re.compile(r"cuda([0-9]{2})([0-9])")
+    for subdir in sorted(split_files_a_list_dict.keys(), reverse=True):
+        sub_a_list = split_files_a_list_dict[subdir]
+        write_project_list_to_disk(whl_dir / subdir / "index.html", projects)
+        write_anchor_list_to_disk(whl_dir / subdir / projects[0] / "index.html", sub_a_list)
+        cuda_match = cuda_re.search(subdir)
+        cuda_ver = f"with CUDA {cuda_match.group(1)}.{cuda_match.group(2)}" if cuda_match else ""
+        vis = "with visualisation" if "vis" in subdir else "without visualistion"
+        descriptions.append([
+            f"{projects[0]} built {cuda_ver} {vis}",
+            f"{uri_base}/{subdir}/",
+            f"{uri_base}/{subdir}/{projects[0]}/",
+        ])
+
+    # Write the root index with some installation instructions
+    write_installation_instructions(args.output_dir / "index.html", projects[0], descriptions)
 
 if __name__ == "__main__":
     main()
